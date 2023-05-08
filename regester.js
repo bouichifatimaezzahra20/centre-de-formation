a50a07e357577ba2ec4ca9d173e45b4c57ebadb3
bouichifatimaezzahra20 <bouichi.fatimaezzahra.solicode@gmail.com>
2023-05-08 16:42:46 +0000

    changes monday

--- regester.js
+++ regester.js
@@ -1,15 +1,75 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIn");
-const container = document.getElementById("container");
+var firstNameInput = document.getElementById("firstName");
+var lastNameInput = document.getElementById("lastName");
+var emailInput = document.getElementById("email");
+var passwordInput = document.getElementById("password");
 
-signUpButton.addEventListener("click", () => {
-	container.classList.add("right-panel-active");
+var firstNameError = document.getElementById("firstName-error");
+var lastNameError = document.getElementById("lastName-error");
+var emailError = document.getElementById("email-error");
+var passwordError = document.getElementById("password-error");
+
+firstNameInput.addEventListener("keyup", function() {
+  if (firstNameInput.validity.valueMissing) {
+    firstNameError.innerHTML = "First name is required";
+  } else {
+    firstNameError.innerHTML = "";
+  }
 });
 
-signInButton.addEventListener("click", () => {
-	container.classList.remove("right-panel-active");
+lastNameInput.addEventListener("keyup", function() {
+  if (lastNameInput.validity.valueMissing) {
+    lastNameError.innerHTML = "Last name is required";
+  } else {
+    lastNameError.innerHTML = "";
+  }
 });
-var hamburger = document.querySelector(".hamburger");
-hamburger.addEventListener("click", function(){
-	document.querySelector("body").classList.toggle("active");
-})
+
+emailInput.addEventListener("keyup", function() {
+  if (emailInput.validity.valueMissing) {
+    emailError.innerHTML = "Email is required";
+  } else if (!validateEmail(emailInput.value)) {
+    emailError.innerHTML = "Invalid email address";
+  } else {
+    emailError.innerHTML = "";
+  }
+});
+
+passwordInput.addEventListener("keyup", function() {
+  if (passwordInput.validity.valueMissing) {
+    passwordError.innerHTML = "Password is required";
+  } else {
+    passwordError.innerHTML = "";
+  }
+});
+
+function validateEmail(email) {
+  var re = /\S+@\S+\.\S+/;
+  return re.test(email);
+}
+
+function validateForm() {
+  if (firstNameInput.validity.valueMissing) {
+    firstNameError.innerHTML = "First name is required";
+    return false;
+  }
+
+  if (lastNameInput.validity.valueMissing) {
+    lastNameError.innerHTML = "Last name is required";
+    return false;
+  }
+
+  if (emailInput.validity.valueMissing) {
+    emailError.innerHTML = "Email is required";
+    return false;
+  } else if (!validateEmail(emailInput.value)) {
+    emailError.innerHTML = "Invalid email address";
+    return false;
+  }
+
+  if (passwordInput.validity.valueMissing) {
+    passwordError.innerHTML = "Password is required";
+    return false;
+  }
+
+  return true;
+}
